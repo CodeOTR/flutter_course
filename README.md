f1ed4e79056d40de0c7e1272db83f1a153ba74df
Joseph Muller <jtmuller5@gmail.com>
2023-12-06 17:56:41 -0500

    hosting setup

--- README.md
+++ README.md
@@ -18,3 +18,27 @@ flutterfire configure -p flutter-course-staging -o lib/config/firebase_options_s
 
 flutterfire configure -p flutter-course-production -o lib/config/firebase_options_production.dart
 
+```
+String environment = const String.fromEnvironment('environment');
+```
+
+firebase init
+
+Hosting: Configure files for Firebase Hosting and (optionally) set up GitHub Action deploys
+
+Use an existing project
+
+=== Hosting Setup
+? Detected an existing Flutter Web codebase in the current directory, should we use this? Yes
+? In which region would you like to host server-side content, if applicable? us-east1 (South Carolina)
+? Set up automatic builds and deploys with GitHub? Yes
+
+Enable Firebase CLI access in GitHub
+
+? Set up the workflow to run a build script before every deploy? Yes
+? What script should be run before every deploy? flutter build web
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_course/config/firebase_options_staging.dart' as staging;
+import 'package:flutter_course/config/firebase_options_production.dart' as production;
 
-void main() {
+String environment = const String.fromEnvironment('environment');
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: environment == 'staging' ? staging.DefaultFirebaseOptions.currentPlatform : production.DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
@@ -9,10 +20,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hello World! ($environment)'),
         ),
       ),
     );
